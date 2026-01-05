6db0e84effbb1676ced7ce2284def0e0c0431c7e
Tobias Paulsen <tobiasrp98@gmail.com>
2026-01-05 15:11:51 +0100

    Focus on random flag button when game over

--- src/lib/components/FlagResultPanel.svelte
+++ src/lib/components/FlagResultPanel.svelte
@@ -12,6 +12,14 @@
     targetCountryState: ITargetCountryState;
     restartGame: () => void;
   } = $props();
+
+  let buttonRef = $state<HTMLButtonElement>();
+
+  $effect(() => {
+    if ((gameWon || gameOver) && buttonRef) {
+      buttonRef.focus();
+    }
+  });
 </script>
 
 {#if gameWon || gameOver}
@@ -36,6 +44,7 @@
       {/if}
     </div>
     <button
+      bind:this={buttonRef}
       onclick={restartGame}
       class="bg-secondary-900 min-h-11 p-2 px-4 rounded self-start text-white font-semibold hover:scale-[1.02] active:scale-95 transition-all disabled:bg-secondary-900/30 disabled:text-secondary-100/50 disabled:cursor-not-allowed"
     >
